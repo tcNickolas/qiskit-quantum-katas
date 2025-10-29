5f7d5aba55a78d29b9ad42277beac4329caeaeb8
Mariia Mykhailova <michaylova@gmail.com>
2025-10-28 20:45:22 -0700

    Add Qubit and SingleQubitGates katas

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# qiskit-quantum-katas
-Qiskit translation of the Quantum Katas
+# Qiskit Quantum Katas
+
+Qiskit translation of the [Quantum Katas](https://quantum.microsoft.com/en-us/tools/quantum-katas).
+
+## Introduction
+
+The Quantum Katas are a collection of self-paced tutorials and programming exercises to help you learn quantum computing and quantum programming.
+
+* Each kata covers one quantum computing topic, introducing you to the basic concepts and algorithms used in quantum computing. The theoretical concepts are supported with programming exercises.
+* Each exercise gives you a task description and a signature of a Python function. Your goal is to implement the function that solves the task.
+* The sequence of exercises progresses from easy to hard. The first exercise might require just one line of code or even a single gate, and the later ones might require rather complicated code.
+* Each kata has a testing framework that sets up, runs, and validates your solutions. Execute the Jupyter Notebook code cell with yours solution to check whether it is correct. Once your code passes the test, you can move on to the next task.
+
+
+## Learning path
+
+### Quantum Computing Concepts: Qubits and Gates
+
+* [The qubit](./Qubit/Qubit.ipynb). Learn what a qubit is.
+* [Single-qubit gates](./SingleQubitGates/SingleQubitGates.ipynb). Learn about what quantum gate is and about the most common single-qubit gates.
---
+++ SingleQubitGates/test_SingleQubitGates.py
@@ -0,0 +1,187 @@
+from cmath import exp
+from math import pi, sqrt, cos, sin
+from functools import partial
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit_aer import AerSimulator
+from qiskit.quantum_info import Operator
+from pytest import approx, mark
+from warnings import catch_warnings
+
+try:
+    from importnb import Notebook
+    # Ignore warnings about invalid syntax when importing LaTeX cells
+    with catch_warnings(action="ignore", category=SyntaxWarning):
+        with Notebook():
+            import Workbook_SingleQubitGates as ref
+    ref_available = True
+except ImportError:
+    ref_available = False
+    # Skip all tests in this file - pytest checks reference solutions and that won't work without these imports
+    pytestmark = mark.skip("No importnb/reference file available")
+
+
+def exercise(fun):
+    test_name = "test_" + fun.__name__
+    try:
+        test_func = globals()[test_name]
+    except KeyError:
+        print(f"Test {test_name} not found")
+    else:
+        try:
+            test_func(fun)
+        except Exception as e:
+            print("Incorrect")
+            print(e)
+        else:
+            print("Correct!")
+    
+    return fun
+
+
+# Create the simulator instance to add save_statevector method to QuantumCircuit
+simulator = AerSimulator(method='statevector')
+
+
+def check_state_vector(
+    fun,            # Callable that is being tested
+    expected_vector # State vector it should prepare
+) -> None:
+    # Construct the circuit that has the callable as a part of it
+    q = QuantumRegister(1)
+    circ = QuantumCircuit(q)
+    fun(circ, q)
+
+    # Save the state vector
+    circ.save_statevector()
+
+    # Run the simulation and get the results
+    res = simulator.run(circ).result()
+    # Extract the saved state vector
+    actual_vector = res.get_statevector().data
+
+    if actual_vector != approx(expected_vector):
+        print("Expected state vector:")
+        print_matrix(expected_vector)
+        print("Actual state vector:")
+        print_matrix(actual_vector)
+        raise ValueError("State vectors should be equal")
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+
+def check_operation_matrix(
+    fun,            # Callable that is being tested
+    expected_matrix # Matrix it should have
+) -> None:
+    # Construct the circuit that has the callable as a part of it
+    q = QuantumRegister(1)
+    circ = QuantumCircuit(q)
+    fun(circ, q)
+    # Convert the circuit to a matrix
+    op = Operator(circ)
+    actual_matrix = op.data
+
+    # Check that the actual matrix matches the expected one
+    for actual, expected in zip(actual_matrix, expected_matrix):
+        if actual != approx(expected):
+            print("Expected matrix:")
+            print_matrix(expected_matrix)
+            print("Actual matrix:")
+            print_matrix(actual_matrix)
+            raise ValueError("Operation matrices should be equal")
+
+
+def test_state_flip(fun=ref.state_flip if ref_available else None):
+    expected_matrix = [[0, 1], [1, 0]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_sign_flip(fun=ref.sign_flip if ref_available else None):
+    expected_matrix = [[1, 0], [0, -1]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_apply_y(fun=ref.apply_y if ref_available else None):
+    expected_matrix = [[0, -1j], [1j, 0]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_sign_flip_on_zero(fun=ref.sign_flip_on_zero if ref_available else None):
+    expected_matrix = [[-1, 0], [0, 1]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_global_phase_minus_one(fun=ref.global_phase_minus_one if ref_available else None):
+    expected_matrix = [[-1, 0], [0, -1]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_global_phase_i(fun=ref.global_phase_i if ref_available else None):
+    expected_matrix = [[1j, 0], [0, 1j]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_basis_change(fun=ref.basis_change if ref_available else None):
+    sqrt12 = 1 / sqrt(2)
+    expected_matrix = [[sqrt12, sqrt12], [sqrt12, -sqrt12]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_prepare_plus(fun=ref.prepare_plus if ref_available else None):
+    sqrt12 = 1 / sqrt(2)
+    expected_vector = [sqrt12, sqrt12]
+    check_state_vector(fun, expected_vector)
+
+
+def test_prepare_minus(fun=ref.prepare_minus if ref_available else None):
+    sqrt12 = 1 / sqrt(2)
+    expected_vector = [sqrt12, -sqrt12]
+    check_state_vector(fun, expected_vector)
+
+
+def test_relative_phase_i(fun=ref.relative_phase_i if ref_available else None):
+    expected_matrix = [[1, 0], [0, 1j]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_relative_phase_three_quarters_pi(fun=ref.relative_phase_three_quarters_pi if ref_available else None):
+    expected_matrix = [[1, 0], [0, exp(3 * pi / 4 * 1j)]]
+    check_operation_matrix(fun, expected_matrix)
+
+
+def test_amplitude_change(fun=ref.amplitude_change if ref_available else None):
+    for ind in range(37):
+        gamma = 2 * pi * ind / 36
+        expected_matrix = [[cos(gamma), -sin(gamma)], [sin(gamma), cos(gamma)]]
+        fun_gamma = partial(fun, gamma=gamma)
+        check_operation_matrix(fun_gamma, expected_matrix)
+
+
+def test_relative_phase_change(fun=ref.relative_phase_change if ref_available else None):
+    for ind in range(37):
+        gamma = 2 * pi * ind / 36
+        expected_matrix = [[1, 0], [0, exp(1j * gamma)]]
+        fun_gamma = partial(fun, gamma=gamma)
+        check_operation_matrix(fun_gamma, expected_matrix)
+
+
+def test_prepare_rotated_state(fun=ref.prepare_rotated_state if ref_available else None):
+    for ind in range(11):
+        alpha = cos(ind)
+        beta = sin(ind)
+        expected_vector = [alpha, -1j * beta]
+        fun_alpha_beta = partial(fun, alpha=alpha, beta=beta)
+        check_state_vector(fun_alpha_beta, expected_vector)
+
+
+def test_prepare_arbitrary_state(fun=ref.prepare_arbitrary_state if ref_available else None):
+    for ind in range(11):
+        alpha = cos(ind)
+        beta = sin(ind)
+        theta = sin(ind * 3)
+        expected_vector = [alpha, exp(1j * theta) * beta]
+        fun_alpha_beta = partial(fun, alpha=alpha, beta=beta, theta=theta)
+        check_state_vector(fun_alpha_beta, expected_vector)
